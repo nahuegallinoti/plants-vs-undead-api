9e57ddc42a5252dd7741e6a858e32a1882f50cbe
nahuegallinoti <nahuel.gallinoti@incluit.com>
2021-08-11 15:41:32 -0300

    Crows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { PlantsService } from './Services/plants-service';
 import { takeUntil } from 'rxjs/operators';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Datum } from './Models/Plant';
 
 
 @Component({
@@ -16,12 +17,13 @@ export class AppComponent implements OnInit, OnDestroy {
   private ngUnsubscribe = new Subject();
 
   searchForm: FormGroup;
-  offset: number = 10;
+  offset: number = 0;
   limit: number = 10;
-  plants: any[] = [];
+  plants: Datum[] = [];
   total: number = 0;
   pages: number = 0;
   paginaActual: number = 1;
+  crowPlants: Datum[] = [];
 
   constructor(
     private _plantsService: PlantsService,
@@ -37,21 +39,50 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
 
-  avanzarPagina() {    
+  avanzarPagina() {
     this.paginaActual += 1;
-    this.offset += 10; //deberia incrementarse despues de hacer el search
+    this.offset += 10;
     this.search();
   }
 
   search() {
     this._plantsService.getPlantsByAddress(this.limit.toString(), this.offset.toString(), this.searchForm.value.address, this.searchForm.value.token).pipe(takeUntil(this.ngUnsubscribe)).subscribe(
-      (result) => {        
+      (result) => {
         this.plants = result.data;
         this.total = result.total;
-        this.pages = Math.trunc(this.total / this.limit) + 1;   
+        this.pages = Math.trunc(this.total / this.limit) + 1;
       },
       (error) => { console.log(error); }
     );
+  }
+  
+  updateOffset = () => {
+    this.offset += 10;
+  }
+
+  updatePage = () => {
+    this.paginaActual +=1;
+  }
+
+  searchCrows() {
+    setInterval(() => {    
+      this._plantsService.getPlantsByAddress(this.limit.toString(), this.offset.toString(), this.searchForm.value.address, this.searchForm.value.token).pipe(takeUntil(this.ngUnsubscribe)).subscribe(
+        (result) => {
+          this.plants = result.data;
+          this.total = result.total;
+          this.pages = Math.trunc(this.total / this.limit) + 1;
+
+          for (let i = 0; i < this.plants.length; i++) {
+            this.plants[i].hasCrow ? this.crowPlants.push(this.plants[i]) : "";
+          }
+
+          this.updateOffset();
+          this.updatePage();
+        },
+        (error) => { console.log(error); }
+      );
+    }, 3000);
+
   }
 
   ngOnDestroy() {
